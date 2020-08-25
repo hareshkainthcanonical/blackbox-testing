f091321bc430057443ffecb7fabd66c4ff209abd
Haresh Kainth <haresh.kainth@canonical.com>
2020-08-25 10:00:43 +0100

    wip: app service configurable environment variable
    App service configurable most use blackbox-tests profile hence setting the environment variable appropriately. This has been taken from the docker test compose file.

--- bin/env.sh
+++ bin/env.sh
@@ -3,4 +3,5 @@
 export appService=edgexfoundry/docker-app-service-configurable:1.2.0
 export postman=postman/newman:4.5.6
 export docker_compose_test_tools=$PWD/docker-compose-test-tools.yml
+export EDGEX_PROFILE=blackbox-tests
 
